2af31b03e14749d5ab5aa01b900177b634e3eca0
anaisateodoro <anaisa_teodoro@estudante.sesisenai.org.br>
2024-06-03 22:48:42 -0300

    add ex. 04 s2

---
+++ aula_03/template-literais.js
@@ -0,0 +1,6 @@
+const livro = {
+    nome: ' Estruturas de Dados e Algoritmos em JavaScript',
+    editora: 'Novatec',
+    paginas: 406
+}
+console.log('O nome do livro é', livro.nome, 'da editora',livro.editora, 'e tem', livro.paginas, 'páginas.Desejo bom estudo com essa dica de leitura!');
